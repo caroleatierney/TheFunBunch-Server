0f133d1a27633e6b504dc8c2d61a878021bafdb0
carole tierney <caroleatierney@gmail.com>
2024-06-12 15:09:23 -0400

    server and db almost done

---
+++ connectDB.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+require('dotenv').config()
+
+const connectDB = async () => {
+    try {
+        console.log(process.env.MONGODB_URI);
+        
+        mongoose.set('strictQuery', false);
+        const conn = await mongoose.connect(process.env.MONGODB_URI);
+        console.log(`Database Connected ${conn.connection.host}`);
+    } catch (error) {
+        console.log(error);
+        process.exit(1); 
+    }
+}
+
+module.exports = connectDB;
---
+++ index.js
@@ -0,0 +1,50 @@
+// ******************************
+// ******* DEPENDENCIES *********
+// ******************************
+require("dotenv").config();
+const cors = require("cors");
+const express = require("express");
+const connectDB = require("./connectDB");
+const TripBlog = require("./models/TripBlog");
+
+// ******************************
+// ******** Express App *********
+// ******************************
+const app = express();
+
+// ******************************
+// ************ PORT ************
+// ******************************
+const PORT = process.env.PORT || 8000;
+
+// ******************************
+// ***********  MONGO ***********
+// ******************************
+connectDB();
+
+// ******************************
+// ********  MIDDLEWARE *********
+// ******************************
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// app.use("/uploads", express.static("uploads"));
+
+// ******************************
+// **********  DATABASE *********
+// ******************************
+
+// ****************************************
+// ************** LISTENER ****************
+// ****************************************
+app.get("/", (req, res) => {
+  res.json("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on Port: ${PORT}`);
+});
+
+app.get("*", (req, res) => {
+  res.sendStatus("404");
+});
